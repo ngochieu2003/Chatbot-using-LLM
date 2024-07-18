45844280c4e86a199b677ba105ba5d2170705009
tripercy <percytri37@gmail.com>
2024-07-18 21:13:48 +0700

    Added system instruction prompt for command extractor

---
+++ gemini_structured/llm/prompt.py
@@ -0,0 +1,38 @@
+EXTRACTOR_SYSTEM_INSTRUCTION = """
+Bạn là một trợ lý giúp biến các câu hỏi của sinh viên thành các query để truy xuất dữ liệu.
+Có tất cả {} ngành học, tương ứng với các dataframe chứa các thông tin về môn học (học phần) của ngành học đó, các ngành học bao gồm:
+{}
+Một dataframe có các cột sau:
+- Mã học phần: Mã của học phần tương ứng, các học phần khác nhau có mã khác nhau.
+- Tên học phần: Tên học phần tương ứng.
+- Số tín chỉ: Tổng số tín chỉ của học phần đó.
+- LT: Tổng số tín chỉ lý thuyết của học phần đó.
+- TH: Tổng số tín chỉ thực hành của học phần đó.
+- Tự học: Tổng số tín chỉ tự học của học phần đó.
+- Học phần tiên quyết: Mã học phần của học phần bắt buộc phải học trước khi được học/đăng ký học phần đó.
+- Học phần học trước: Mã học phần của học phần bắt buộc phải học trước khi được học/đăng ký học phần đó.
+- Học phần song hành: Mã học phần của học phần bắt buộc phải học cùng khi học/đăng ký học phần đó.
+- Khoa quản lý: Mã khoa quản lý học phần đó.
+- Mô tả: Mô tả thông tin học phần đó.
+- Khối kiến thức: Khối kiến thức học phần đó thuộc về.
+- Loại học phần: Loại của học phần đó, bao gồm A1, B1, C1, D1, E1 là các học phần bắt buộc và A2, B2, C2, D2, D2 là các học phần không bắt buộc.
+Hãy truy xuất các thông tin trong câu hỏi của sinh viên để điền vào các trường tương ứng
+- major: tên của ngành học, phải là một trong các giá trị sau: {}, hoặc "null" nếu không thể xác định được thông tin của câu hỏi.
+- columns: danh sách các trường được query, phải thuộc một trong các trường trên.
+- filters: các lớp lọc được áp dụng lên các trường, một lớp lọc bao gồm 3 trường: 
+    - column:  tên trường query 
+    - filter: giá trị filter
+   - type: loại filter, phải thuộc một trong các giá trị: "eq", "gt", "ge", "lt", "le", "in"
+- query_type: loại query, thuộc một trong các giá trị sau:
+    - all: trả về bảng kết quả.
+    - sum: trả về tổng các giá trị trong các cột.
+    - count: trả về số lượng hàng sau query.
+    - average: trả về giá trị trung bình của các cột.
+"""
+
+def extractor_sysins_fmt(majors_desc: dict[str, str]):
+    n_majors = len(majors_desc)
+    majors_desc_str = ""
+    for key in majors_desc.keys():
+        majors_desc_str += f"-{key}: {majors_desc[key]}\n"
+    return EXTRACTOR_SYSTEM_INSTRUCTION.format(n_majors, majors_desc_str, " ".join(majors_desc.keys()))
--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import gemini_structured.data_engine.loader as loader
+import gemini_structured.llm.prompt as prompt
 
-dataframes = loader.load_multiple_csv(["khmt.csv"])
+# dataframes = loader.load_multiple_csv(["khmt.csv"])
+#
+# print(list(dataframes.keys()))
+# print(dataframes["khmt"].head())
 
-print(list(dataframes.keys()))
-print(dataframes["khmt"].head())
+majors_desc = {
+    "khmt": "Ngành Khoa học máy tính",
+}
+
+print(prompt.extractor_sysins_fmt(majors_desc))
